7d7f5f2793209eaf424c5e3f6e2dca1165208290
Laurent Fintoni <72798408+laurentfintoni@users.noreply.github.com>
2022-02-16 13:34:44 +0100

    testing

--- scraper.py
+++ scraper.py
@@ -1,6 +1,8 @@
-import requests
-import json
+from __future__ import print_function
 from bs4 import BeautifulSoup
+import json
+import urllib
+import requests
 
 """ URL = "https://www.moma.org/collection/works/10"
 page = requests.get(URL)
@@ -29,7 +31,7 @@ print(moma_text) """
 
 #rhizome script 
 
-def url_to_text_rhizome(url):
+""" def url_to_text_rhizome(url):
     page = requests.get(url).text
     soup = BeautifulSoup(page, "lxml")
     text = [p.text for p in soup.find(class_="mw-parser-output").find_all('p')]
@@ -38,16 +40,35 @@ def url_to_text_rhizome(url):
 
 #rhizome urls 
 
-""" with open('query.json') as file:
-    rhizome_dump = json.load(file) """
+with open('query.json') as file:
+    rhizome_dump = json.load(file)
 
 urls_2 = [link['document_url'] for link in rhizome_dump]
 
 rhizome_text = [url_to_text_rhizome(u) for u in urls_2]
 
-print(len(rhizome_text))
+print(len(rhizome_text)) """
 
 #query ttl dump for artwork label, description label, and documentl_url 
 
+#google searches
+
+"""Example of Python client calling Knowledge Graph Search API."""
+
+
+api_key = 'AIzaSyDg0OU1DRA8ApbW0mqwBZ64eAPyaSYQy-I'
+query = 'Mark Tribe'
+service_url = 'https://kgsearch.googleapis.com/v1/entities:search'
+params = {
+    'query': query,
+    'limit': 5,
+    'indent': True,
+    'key': api_key,
+}
+url = service_url + '?' + urllib.parse.urlencode(params)
+response = json.loads(urllib.request.urlopen(url).read())
+for element in response['itemListElement']:
+    if 'artist' in element['result']:
+        print(element['result'])
 
 #what format do we want the divs into? 
