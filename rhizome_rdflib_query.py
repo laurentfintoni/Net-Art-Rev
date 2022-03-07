7715a3b55448e13462a3cac3e3e82a1e72000052
Laurent Fintoni <72798408+laurentfintoni@users.noreply.github.com>
2022-03-07 13:38:08 +0100

    rhizome dataset updates

--- rhizome_rdflib_query.py
+++ rhizome_rdflib_query.py
@@ -8,13 +8,38 @@ g = rdflib.ConjunctiveGraph()
 result = g.parse("Rhizome_data/Rhizome-ArtBase_2022-02-14.ttl", format="ttl")
 
 query_results = g.query(
-    """SELECT ?artwork_label ?description_label ?document_url {
-  wd:Q1292 rdfs:label ?artwork_label .
-  wd:Q1292 wdt:P123 ?description .
-  ?document_url schema:about ?description .
-  ?description rdfs:label ?description_label .
-}
-LIMIT 100""")
+    """SELECT ?artwork_page ?artwork_label ?artist_label ?accession ?inception ?summary_url ?description_url ?statement_url
+{ 
+  ?artwork rdfs:label ?artwork_label ;
+   wdt:P3 wd:Q5 ;
+   wdt:P29 ?artist .
+   OPTIONAL {
+   ?artwork wdt:P85 ?accession ;
+           wdt:P26 ?inception .}
+  OPTIONAL {
+  ?artist rdfs:label ?artist_label .}
+  OPTIONAL {
+  ?artwork_page schema:about ?artwork .}
+  OPTIONAL {
+  ?artwork wdt:P123 ?summary_description .}
+  OPTIONAL {
+  ?summary_description wdt:P3 wd:Q4985 ;
+          rdfs:label ?summary_label .}
+  OPTIONAL {
+  ?summary_url schema:about ?summary_description .}
+  OPTIONAL {
+  ?artwork wdt:P123 ?description .
+  ?description wdt:P3 wd:Q9759 ;
+          rdfs:label ?description_label .}
+  OPTIONAL {
+  ?description_url schema:about ?description .}
+  OPTIONAL {
+    ?artwork wdt:P123 ?statement .
+  ?statement wdt:P3 wd:Q11838 ;
+          rdfs:label ?statement_label .}
+  OPTIONAL {
+  ?statement_url schema:about ?statement .}
+}""")
 
 for query_res in query_results:
   print(query_res) # notice the two alternative ways to recall values in the tuple
